c989129e60b5b809ca9efccf28cf7f8dd2b68a50
cooperkinnan <cooperkinnan@unomaha.edu>
2025-02-24 03:35:10 +0000

    done with lab

--- WordGame.py
+++ WordGame.py
@@ -1,15 +1,22 @@
-#Word Game is a knock-off version of a popular online word-guessing game.
+#Name:Cooper Kinnan
+#Date:2/23/2025
+#Assignment:Lab
+
+
+
 
 import random
 
 def inWord(letter, word):
     """Returns boolean if letter is anywhere in the given word"""
-
+    if word.find(letter) >=0:
+        return True
     return False
 
 def inSpot(letter, word, spot):
     """Returns boolean response if letter is in the given spot in the word."""
-
+    if letter == word[spot]:
+        return True
     return False
 
 def rateGuess(myGuess, word):
@@ -17,7 +24,16 @@ def rateGuess(myGuess, word):
     - Capital letter if the letter is in the right spot
     - Lower case letter if the letter is in the word but in the wrong spot
     - * if the letter is not in the word at all"""
-
+    feedback =""
+    for spot in range (5):
+        letter = myGuess[spot]
+        if inSpot(letter, word, spot) == True:
+            feedback = feedback + letter.upper()
+        elif inWord(letter, word) == True:
+            feedback = feedback + letter.lower()
+        else:
+            feedback = feedback +"*"
+    return feedback
 
 def main():
     #Pick a random word from the list of all words
@@ -25,8 +41,27 @@ def main():
     content = wordFile.read()
     wordList = content.split("\n")
     todayWord = random.choice(wordList)
-    print(todayWord)
+    #print(todayWord)
+    guess =""
+    count = 0
+    while guess != todayWord and count <6:
+        goodGuess = False
+        while goodGuess == False:
+            guess = input("Enter word:")
+            if guess in wordList:
+                count = count + 1
+                goodGuess = True
+            else:
+                print("Invalid Word:")
+    
+        feedback = rateGuess(guess, todayWord)
+        print(feedback)
+    if count < 6:
+        print("YAY")
+    else:
+        print("Boo")
 
+    print("The word was", todayWord)
     #User should get 6 guesses to guess
 
     #Ask user for their guess
